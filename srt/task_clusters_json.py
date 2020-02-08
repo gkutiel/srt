c2598aeccfd1e0e339c2261f2cc73e4cb88cfc2b
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-02-08 11:46:18 +0200

    clusters

--- srt/task_clusters_json.py
+++ srt/task_clusters_json.py
@@ -1,7 +1,9 @@
-import json
-from srt.paths import *
 import fasttext
+from pandas import DataFrame
 from sklearn.cluster import MiniBatchKMeans
+from sklearn.manifold import TSNE
+
+from srt.paths import *
 
 
 def clusters_json():
@@ -11,8 +13,30 @@ def clusters_json():
         random_state=2020,
     )
 
-    labels = list(kmean.fit_predict([model[word] for word in model.words]))
-    labels = [int(label) for label in labels]
+    vecs_100 = [model[word] for word in model.words]
+    labels = list(kmean.fit_predict(vecs_100))
 
-    with open(out.clusters_json, 'w') as f:
-        json.dump(list(zip(model.words, labels)), f)
+    tsne = TSNE(random_state=2020)
+    vecs_2 = tsne.fit_transform(vecs_100)
+
+    clusters = [
+        {
+            'words': word,
+            'label': label,
+            'vec_100': vec_100,
+            'vec_2': vec_2,
+        }
+
+        for word, label, vec_100, vec_2 in zip(
+            model.words,
+            labels,
+            vecs_100,
+            vecs_2
+        )
+    ]
+
+    DataFrame(clusters).to_json(
+        out.clusters_json,
+        lines=True,
+        orient='records',
+    )
