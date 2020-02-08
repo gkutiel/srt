fdb3ced3a767d0e3017ee0a270bf6d03d0a9124e
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-02-08 09:55:39 +0200

    data

--- srt/paths.py
+++ srt/paths.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 
 
+class srt:
+    _ = Path('srt')
+
+    task_srt_2_txt = _ / 'task_srt_2_txt.py'
+
+
 class data:
     _ = Path('data')
+
+    data_txt = _ / 'data.txt'
